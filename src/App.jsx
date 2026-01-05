b1bb1ac4e883e0dc3571865f6fda4b117cbb0d28
Jeremy Price <jeremyprice@Jeremys-MacBook-Pro-2.local>
2026-01-05 14:28:57 -0700

    App.jsx - add subhead

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Schedule your Tasks</h1>
+        <h1 className="main-heading">Team Planning <span>🗓️</span></h1>
+        <p>Schedule tasks to be completed by a specific date.</p>
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
