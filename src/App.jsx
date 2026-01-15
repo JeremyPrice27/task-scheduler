dc3191ecc833eb9ef286c2c22aca4753b75eaba2
Jeremy Price <jeremyprice27@gmail.com>
2026-01-15 11:27:39 -0700

    Add TaskList component to the main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import TaskList from './TaskList';
 
 function App() {
   return (
@@ -7,8 +8,8 @@ function App() {
         <h1 className="main-heading">Team Planning <span>🗓️</span></h1>
         <p>Schedule tasks to be completed by a specific date.</p>
       </header>
-      <main>
-
+      <main className="main-section">
+        <TaskList />
       </main>
     </div>
   );
