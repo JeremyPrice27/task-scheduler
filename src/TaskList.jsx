6db9fd739c38a6777995b737753d899c9fd4ce78
Jeremy Price <jeremyprice27@gmail.com>
2026-01-20 11:56:29 -0700

    Refactor TaskList to use Redux for task management

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,31 +1,59 @@
+import { useDispatch, useSelector } from "react-redux";
+import { completeTask, removeTask, selectTaskList } from "./taskList";
 function TaskList() {
-    const tasksData = [
-        { id: 1, description: 'Learn React hooks', dueDate: '2025-02-15', completed: true },
-        { id: 2, description: 'Build a task table component', dueDate: '2025-03-01', completed: false },
-        { id: 3, description: 'Style the table with CSS', dueDate: '2025-03-10', completed: false },
-    ];
-  return (
-    <div className="task-list">
-        <table className="task-table">
-        <thead>
-            <tr>
-            <th>Description</th>
-            <th>Due Date</th>
-            <th>Completed</th>
-            </tr>
-        </thead>
-        <tbody>
-            {tasksData.map((task) => (
-            <tr key={task.id}>
-                <td>{task.description}</td>
-                <td>{task.dueDate}</td>
-                <td>{task.completed ? 'Yes' : 'No'}</td>
-            </tr>
-            ))}
-        </tbody>
-        </table>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const taskListData = useSelector(selectTaskList);
+    const onCompleteTask = function(id) {
+        dispatch(completeTask(id));
+    };
+    const onRemoveTask = function(id) {
+        dispatch(removeTask(id));
+    };
+    return (
+        <>
+        {taskListData.length == 0 && 
+            <p>There are no available tasks at this time.</p>
+        }
+        {taskListData.length > 0 && 
+        <div className="task-list">
+            <table className="task-table">
+            <thead>
+                <tr>
+                <th>Description</th>
+                <th>Due Date</th>
+                <th>Completed</th>
+                <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {taskListData.map((task) => (
+                <tr key={task.id}>
+                    <td>{task.description}</td>
+                    <td>{task.dueDate}</td>
+                    <td>
+                        {task.completed &&
+                        <span className="completed">Yes</span>
+                        }
+                        {!task.completed &&
+                        <span>No</span>
+                        }
+                    </td>
+                    <td>
+                        {task.completed &&
+                        <button className="action-button" onClick={() => onRemoveTask(task.id)}>Remove</button>
+                        }
+                        {!task.completed &&
+                        <button className="action-button complete" onClick={() => onCompleteTask(task.id)}>Complete</button>
+                        }
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+        }
+        </>
+    );
 }
 
 export default TaskList;
